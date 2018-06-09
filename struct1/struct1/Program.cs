568b6e2bc275b020c06ec679565267f67ed032cb
jul-ia <julie78sd@gmail.com>
2018-06-09 19:38:19 +0300

    Done

--- struct1/struct1/Program.cs
+++ struct1/struct1/Program.cs
@@ -8,9 +8,68 @@ namespace struct1
 {
     class Program
     {
+
+        public struct Train
+        {
+            public string dest;
+            public int num;
+            public string time;
+
+           
+            public void show()
+            {
+                Console.WriteLine("Train No {0}", num);
+                Console.WriteLine("Destination: {0}", dest);
+                Console.WriteLine("Departure at {0}", time);
+                Console.WriteLine(new string('*', 30));
+
+            }
+        }
+
+        public static int Comp(Train t1, Train t2)
+        {
+             return t1.num.CompareTo(t2.num);
+        }
+
+
+        public static int check(Train[] t, int n)
+        {
+            for (int i = 0; i < t.Length; i++)
+                if (t[i].num == n)
+                    return i;
+            return -1;
+        }
+
         static void Main(string[] args)
         {
+            Train[] trains = new Train[3];
+            for(int i = 0; i < trains.Length; i++)
+            {
+                Console.Write("Enter destination: ");
+                trains[i].dest = Console.ReadLine();
+                Console.Write("Enter train number: ");
+                int.TryParse(Console.ReadLine(), out trains[i].num);
+                Console.Write("Enter time of departure: ");
+                trains[i].time = Console.ReadLine();
+                Console.WriteLine(new string('-', 30));
+            }
+            Console.WriteLine();
+            Array.Sort(trains, Comp);
 
+
+            for (int i = 0; i < trains.Length; i++)
+                trains[i].show();
+
+            int n;
+            Console.Write("\nEnter number of train: ");
+            int.TryParse(Console.ReadLine(), out n);
+            int ind = check(trains, n);
+            if (ind != -1)
+                trains[ind].show();
+            else
+                Console.WriteLine("Train is not found.");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ struct1/struct2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct2
+{
+    class Program
+    {
+
+        class MyClass
+        {
+            public string change;
+        }
+
+        struct MyStruct
+        {
+            public string change;
+        }
+
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "MyClass: Changed";
+        }
+
+        static void StruktTaker(MyStruct myStruct)
+        {
+            myStruct.change = "MyStruct: Changed";
+        }
+
+
+        static void Main(string[] args)
+        {
+            MyClass m = new MyClass();
+            m.change = "MyClass: Not changed";
+            MyStruct s;
+            s.change = "MyStruct: Not changed";
+
+            Console.WriteLine("Before:");
+            Console.WriteLine(m.change);
+            Console.WriteLine(s.change);
+
+            Console.WriteLine("\nAfter:");
+            ClassTaker(m);
+            StruktTaker(s);
+            Console.WriteLine(m.change);
+            Console.WriteLine(s.change);
+
+
+            Console.ReadLine();
+        }
+    }
+}
